2597bcc2036496c20f718cb524b1442a1de75c28
singleMaltMatt <mattmatte93@gmail.com>
2022-03-26 17:58:05 +0200

    creating grid

--- js/app.js
+++ js/app.js
@@ -1,28 +1,63 @@
 const cardArray = [
     {
         name: 'cherry',
-        img: '../img/cherry.png',
+        img: 'img/cherry.png'
     },
     {
         name: 'chest',
-        img: '../img/chest.png',
+        img: 'img/chest.png'
     },
     {
         name: 'chicken',
-        img: '../img/chicken.png'
+        img: 'img/chicken.png'
     },
     {
         name: 'heart',
-        img: '../img/heart.png'
+        img: 'img/heart.png'
     },
     {
         name: 'skull',
-        img: '../img/skull.png',
+        img: 'img/skull.png'
     },
     {
         name: 'star',
-        img: '../img/star.png'
+        img: 'img/star.png'
     },
-    {}
-
+    {
+        name: 'cherry',
+        img: 'img/cherry.png'
+    },
+    {
+        name: 'chest',
+        img: 'img/chest.png'
+    },
+    {
+        name: 'chicken',
+        img: 'img/chicken.png'
+    },
+    {
+        name: 'heart',
+        img: 'img/heart.png'
+    },
+    {
+        name: 'skull',
+        img: 'img/skull.png'
+    },
+    {
+        name: 'star',
+        img: 'img/star.png'
+    },
 ]
+
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector('#grid');
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'img/blank.png')
+        console.log(card, i)
+    }
+}
+createBoard()
