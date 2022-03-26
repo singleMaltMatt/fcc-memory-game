8917ea7255334483c170ed4f3f2975b9bc04773e
singleMaltMatt <mattmatte93@gmail.com>
2022-03-26 16:31:23 +0200

    added images. scripting

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const cardArray = [
+    {
+        name: 'cherry',
+        img: '../img/cherry.png',
+    },
+    {
+        name: 'chest',
+        img: '../img/chest.png',
+    },
+    {
+        name: 'chicken',
+        img: '../img/chicken.png'
+    },
+    {
+        name: 'heart',
+        img: '../img/heart.png'
+    },
+    {
+        name: 'skull',
+        img: '../img/skull.png',
+    },
+    {
+        name: 'star',
+        img: '../img/star.png'
+    },
+    {}
+
+]
