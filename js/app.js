0ba28c8088d006ca47a0c4c2252cd871f49e5efd
singleMaltMatt <mattmatte93@gmail.com>
2022-03-26 18:47:53 +0200

    scripting

--- js/app.js
+++ js/app.js
@@ -53,11 +53,39 @@ cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
 
+const cardsChosen = [];
+const cardsChosenIds = [];
+
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'img/blank.png')
-        console.log(card, i)
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
     }
 }
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('you found a match!')
+        cards[cardsChosenIds[0]].setAttribute('src', 'img/white.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'img/white.png')
+
+    }
+}
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+
+}
+
+
